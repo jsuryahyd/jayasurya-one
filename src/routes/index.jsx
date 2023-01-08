ae9dbda8bb8cef36c95251158d6c016ea9b79f32
jsuryahyd <jaysuryahyd@gmail.com>
2023-01-08 21:43:50 +0530

    first commit after solidstart

---
+++ src/routes/index.jsx
@@ -0,0 +1,105 @@
+import logo from "./logo.svg";
+import styles from "./homepage.module.css";
+import linkedInImage from "../assets/img/linkedin-original.svg";
+import fbSvg from "../assets/img/facebook-original.svg";
+import twitterSvg from "../assets/img/twitter-original.svg";
+import githubSvg from "../assets/img/github-original.svg";
+import soSvg from "../assets/img/stackoverflow.svg";
+import envelope from "../assets/img/envelope.svg";
+
+export default function HomePage(){
+	return <div class={styles.App}>
+	<header class={styles.header}>
+		<a
+			target="_blank"
+			href="https://docs.google.com/document/d/1OXNuIQdYByfBYvVxUXxjmP0cVLzuAt5SXSpJ_SpW71U/edit?usp=sharing"
+		>
+			Jaya Surya
+		</a>
+		<a
+			target="_blank"
+			class={styles["resume-link"]}
+			href="https://drive.google.com/file/d/15W_VI0AolmRHb4tC0LEhkMpJH1gU3T4K/view"
+		>
+			Resume / CV
+		</a>
+	</header>
+	<main class={styles.main}>
+		<div class={styles["content"]}>
+			<h2>Senior Full Stack Developer</h2>
+			<div class={styles.skills} style={{ display: "flex" }}>
+				<span>Nodejs</span>
+				<span>MySql</span>
+				<span>HTML & CSS</span>
+				<span>React</span>
+				<span>React Native</span>
+				<span>Chrome Extensions</span>
+			</div>
+			{/* <div class={styles.skills} style={{ display: "flex" }}>
+				<span>Server Side applications</span>
+				<span>Mobile Applications</span>
+				<span>Web Development</span>
+			</div> */}
+			{/* <div class={styles.skills} style={{ display: "flex" }}>
+				<span>Server Side applications</span>
+				<span>Mobile Applications</span>
+				<span>Web Development</span>
+			</div> */}
+		</div>
+	</main>
+	<footer>
+		<a
+			target="_blank"
+			rel="noreferrer noopener"
+			href="https://github.com/jsuryahyd"
+		>
+			<img src={githubSvg} />
+		</a>
+		<a
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://stackoverflow.com/users/7314900/jay-surya"
+		>
+			{/* linkedin */}
+			<img src={soSvg} />
+		</a>
+		<a
+			target="_blank"
+			rel="noreferrer  noopener"
+			href="https://twitter.com/jaysuryahyd"
+		>
+			<img src={twitterSvg} />
+		</a>
+		<a
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://www.facebook.com/profile.php?id=100027093039467"
+		>
+			<img src={fbSvg} />
+		</a>
+		<a
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://www.linkedin.com/in/jaya-surya-b67a51206/"
+		>
+			{/* linkedin */}
+			<img src={linkedInImage} />
+		</a>
+
+		<a
+			target="_blank"
+			rel="noopener noreferrer"
+			href="mailto:jaysuryahyd@gmail.com"
+			style={{
+				"margin-left": "auto",
+				"font-size": "1.5rem",
+				display: "flex",
+				"align-items": "center",
+			}}
+		>
+			<img src={envelope} style={{ "margin-right": "1rem" }} />
+			<span>jaysuryahyd@gmail.com</span>
+		</a>
+	</footer>
+</div>
+}
---
+++ src/routes/tools/codegen/forms.tsx
@@ -0,0 +1,186 @@
+import { createSignal, For, Match, Switch } from "solid-js";
+import { createStore } from "solid-js/store";
+type inputTypes = "text" | "number" | "email" | "checkbox" | "radio" | "select";
+export default function FormCodeGen() {
+  const [getFormName, setFormName] = createSignal("");
+  const [getFieldType, setFieldType] = createSignal<inputTypes>("text");
+  const list = ["2", "4"];
+  const [formFields, setFormFields] = createStore<{
+    fields: {
+      name: string;
+      value: string;
+      type: inputTypes;
+      options: string[];
+    }[];
+  }>({
+    fields: [],
+  });
+  let input: HTMLInputElement | null = null;
+  let optionsInputRef: HTMLInputElement | null = null;
+  return (
+    <>
+      <select
+        onChange={(e) => setFieldType(e.currentTarget.value as inputTypes)}
+      >
+        <option value="text">Text input</option>
+        <option value="number">Number input</option>
+        <option value="email">Email input</option>
+        <option value="checkbox">Checkbox (Multiple selections)</option>
+        <option value="radio">Radio (Single selection)</option>
+        <option value="select">Selection Dropdown</option>
+      </select>
+      <input type="text" ref={optionsInputRef} placeholder="options" />
+      <input
+        type="text"
+        ref={input}
+        onChange={(e) => setFormName(e.currentTarget.value)}
+        value={getFormName()}
+      />
+      <button
+        onClick={() => {
+          const options = optionsInputRef.value?.split(",");
+          setFormFields((f) => ({
+            fields: [
+              ...f.fields,
+              {
+                name: getFormName(),
+                value: "",
+                options: options,
+                type: getFieldType() || "text",
+              },
+            ],
+          }));
+          if (input) {
+            input.value = "";
+            input.focus();
+          }
+          if (optionsInputRef) {
+            optionsInputRef.value = "";
+          }
+        }}
+        style={{ "margin-left": "1rem" }}
+      >
+        Add field
+      </button>
+      <br />
+      <pre>{JSON.stringify(formFields.fields, null, 2)}</pre>
+      <For each={formFields.fields} fallback={<div>Add an item...</div>}>
+        {(item, index) => (
+          <div style={{ "margin-bottom": "0.5rem" }}>
+            <FormField
+              name={item.name}
+              value={item.value}
+              type={item.type}
+              options={item.options}
+              onChange={(val: any) => {
+                setFormFields(
+                  "fields",
+                  (f) => f.name === item.name,
+                  "value",
+                  (v) => val
+                );
+              }}
+              lable={item.name}
+              onLabelChange={(v: string) =>
+                setFormFields((store) => {
+                  return {
+                    ...store,
+                    fields: store.fields.map((f) =>
+                      f.name === item.name ? { ...f, name: v } : f
+                    ),
+                  };
+                })
+              }
+            />
+          </div>
+        )}
+      </For>
+    </>
+  );
+}
+
+function FormField(props: {
+  name: string;
+  value: string;
+  onChange: Function;
+  type: inputTypes;
+  lable: string;
+  options: string[];
+  onLabelChange: Function;
+}) {
+  console.log(props);
+  return (
+    <label>
+      {JSON.stringify(props)}
+      <input
+        style={{
+          background: "transparent",
+          display: "block ",
+          border: "none",
+          color: "#fff",
+        }}
+        onchange={(e) => props.onLabelChange(e.currentTarget.value)}
+        value={props.lable || ""}
+      />
+      <Switch
+        fallback={
+          <input
+            name={props.name}
+            value={props.value}
+            type={props.type}
+            onChange={(e) => props.onChange(e.currentTarget.value)}
+            placeholder="Enter"
+          />
+        }
+      >
+        <Match
+          when={
+            props.type == "text" ||
+            props.type === "number" ||
+            props.type === "email"
+          }
+        >
+          <input
+            name={props.name}
+            value={props.value}
+            type={props.type}
+            onChange={(e) => props.onChange(e.currentTarget.value)}
+            placeholder="Enter"
+          />
+        </Match>
+        <Match when={props.type === "checkbox"}>
+          {(props.options || []).map((o) => {
+            return (
+              <>
+                <input
+                  type="checkbox"
+                  name={props.name}
+                  value={o}
+                  id={o}
+                ></input>
+                <label htmlFor={o}>{o}</label>
+              </>
+            );
+          })}
+        </Match>
+        <Match when={props.type === "radio"}>
+          {(props.options || []).map((o) => {
+            return (
+              <>
+                <input type="radio" name={props.name} id={o} value={o}></input>{" "}
+                <label htmlFor={o}>{o}</label>
+              </>
+            );
+          })}
+        </Match>
+        <Match when={props.type === "select"}>
+          <select name={props.name}>
+            {(props.options || []).map((o) => {
+              return <option value={o}>{o}</option>;
+            })}
+          </select>
+        </Match>
+      </Switch>
+    </label>
+  );
+}
