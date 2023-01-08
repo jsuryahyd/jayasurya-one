b0432d3be1e0e54b0866fba20400a6eb65a15c11
jsuryahyd <jaysuryahyd@gmail.com>
2023-01-08 23:07:37 +0530

    added ssg, and changed workflow to run on tag

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import solid from "solid-start/vite";
+import ssgAdapter from "solid-start-static";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       enforce: "pre",
     },
     solid({
+      adapter:ssgAdapter(),
       extensions: [".mdx", ".md"],
     }),
   ],
