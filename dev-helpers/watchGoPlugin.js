948a574cd2d9347ac77068de53be4b3b350933da
jsuryahyd <jaysuryahyd@gmail.com>
2024-08-18 16:44:47 +0530

    added log to check the platform

--- dev-helpers/watchGoPlugin.js
+++ dev-helpers/watchGoPlugin.js
@@ -6,10 +6,10 @@ export default function watchGoPlugin() {
     name: 'watch-go-plugin',
     configureServer(server) {
       server.watcher.add(path.resolve(__dirname, 'wasm', 'go-wasm', 'main.go'));
-
+      console.log('platform', process.platform)
       server.watcher.on('change', (file) => {
         if (file.endsWith('main.go')) {
-          exec('/bin/bash./dev-helpers/wasm-builder.sh', (err, stdout, stderr) => {
+          exec('/bin/bash ./dev-helpers/wasm-builder.sh', (err, stdout, stderr) => {
             if (err) {
               console.error(`Error compiling Go code: ${stderr}`);
             } else {
